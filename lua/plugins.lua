7aa7750abfddc692c46c9ae4615c667030c390e6
LutzGebelman <yuraosipov2012@gmail.com>
2023-10-03 22:59:31 +0300

    Added bootstrap for lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 plugins = {
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
@@ -52,10 +65,10 @@ plugins = {
         build = "cd app && npm install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
-            vim.g.mkdp_auto_start = 1
         end,
         ft = { "markdown" },
     },
+    {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
     {
         'nvim-orgmode/orgmode',
         dependencies = {
