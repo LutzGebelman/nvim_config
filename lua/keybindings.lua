ee036077fa85a7281d86fddcded509ed8b8aed2b
LutzGebelman <yuraosipov2012@gmail.com>
2023-10-13 00:46:18 +0300

    Added a Y and YY keybind for system clipboard, cleaned up the cope and added more comments

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,58 +4,59 @@ keymap.terminal = {
     win_id = nil
 }
 
-opts = {
-  mode = "n", -- NORMAL mode
--- prefix: use "<leader>f" for example for mapping everything related to finding files
-  -- the prefix is prepended to every mapping part of `mappings`
-  prefix = "",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = false, -- use `nowait` when creating keymaps
-  expr = false, -- use `expr` when creating keymaps
-}
+-- WARN: No clue what this is for, so gona leave this here untill something breaks
+
+-- local opts = {
+--     mode = "n", -- NORMAL mode
+--     -- prefix: use "<leader>f" for example for mapping everything related to finding files
+--     -- the prefix is prepended to every mapping part of `mappings`
+--     prefix = "",
+--     buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+--     silent = true, -- use `silent` when creating keymaps
+--     noremap = true, -- use `noremap` when creating keymaps
+--     nowait = false, -- use `nowait` when creating keymaps
+--     expr = false, -- use `expr` when creating keymaps
+-- }
+
 
 keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>')
-keymap.set('v', '<Tab>', ":s/./\\t\\0/<CR>:nohl<CR>")
-keymap.set('v', '<S-Tab>', ":s/\\t//<CR>:nohl<CR>")
 
--- Commenting
+-- Tabulation
+keymap.set('v', '<Tab>', ":s/./\\t\\0/<CR>:nohl<CR>") -- Add a tab to each selected line
+keymap.set('v', '<S-Tab>', ":s/\\t//<CR>:nohl<CR>") -- Remove a tab from each selected line
 
-keymap.set('v', 'm"', ':s/\\%V.\\+\\%V/"\\0"<CR>:nohl<CR>')
-keymap.set('v', 'm{', ':s/\\%V.\\+\\%V/{\\0}<CR>:nohl<CR>')
-keymap.set('v', 'm(', ':s/\\%V.\\+\\%V/(\\0)<CR>:nohl<CR>')
-keymap.set('v', 'm[', ':s/\\%V.\\+\\%V/[\\0]<CR>:nohl<CR>')
-keymap.set('v', 'm<', ':s/\\%V.\\+\\%V/<\\0><CR>:nohl<CR>')
-keymap.set('v', "m'", ":s/\\%V.\\+\\%V/'\\0'<CR>:nohl<CR>")
-keymap.set('n', '<F8>', ":lua require'dap'.toggle_breakpoint()<CR>")
-keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>")
-keymap.set('n', '<F6>', function ()
-    require('dap.ext.vscode').load_launchjs('${workspaceFolder}/launch.json')
-end)
-keymap.set('n', '<F10>', ":lua require'dap'.step_over()<CR>")
-keymap.set('n', '<F11>', ":lua require'dap'.step_into()<CR>")
-keymap.set('n', 'd-o', ":lua require'dap'.repl.open()<CR>")
-keymap.set('n', '<esc>', ":sil nohl<CR>")
-keymap.set('t', '<esc>', "<C-\\><C-n>")
-keymap.set({'n', 'v'}, 'D', "\"_d")
-keymap.set({'n', 'v'}, 'DD', "\"_dd")
+-- NOTE: I have no use for this for now
+-- keymap.set('n', '<F8>', ":lua require'dap'.toggle_breakpoint()<CR>")
+-- keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>")
+-- keymap.set('n', '<F6>', function ()
+--     require('dap.ext.vscode').load_launchjs('${workspaceFolder}/launch.json')
+-- end)
+-- keymap.set('n', '<F10>', ":lua require'dap'.step_over()<CR>")
+-- keymap.set('n', '<F11>', ":lua require'dap'.step_into()<CR>")
+-- keymap.set('n', 'd-o', ":lua require'dap'.repl.open()<CR>")
+
+keymap.set('n', '<esc>', ":sil nohl<CR>") -- Dismiss the highlight 
+keymap.set('t', '<esc>', "<C-\\><C-n>") -- Exit terminal input on ESC
+
+-- Buffers
+keymap.set({'n', 'v'}, 'Y', "\"+y") -- Copy to systemwide clipboard
+keymap.set({'n', 'v'}, 'YY', "\"+yy") -- Shortcut to copy line to the systemwide clipboard
+keymap.set({'n', 'v'}, 'D', "\"_d") -- Delete without copy
+keymap.set({'n', 'v'}, 'DD', "\"_dd") -- Shortcut to delete line without copy
 
 
 -- Movement
--- Betwin windows
+-- Window Navigation
 keymap.set('n', 'H', ':wincmd h<CR>')
 keymap.set('n', 'J', ':wincmd j<CR>')
 keymap.set('n', 'K', ':wincmd k<CR>')
 keymap.set('n', 'L', ':wincmd l<CR>')
--- Up/down half page and start/end of a line
+
+-- Up/down half page
 keymap.set('n', '<C-j>', '<C-d>zz')
 keymap.set('n', '<C-k>', '<C-u>zz')
--- keymap.set('n', '<C-h>', '<home>')
--- keymap.set('n', '<C-l>', '<end>')
 
 -- tabs controll
-keymap.set('n', '<leader-e>', ':echo "fuck"')
 keymap.set('n', 'tt', ':tabnew<CR>')
 keymap.set('n', 'tq', ':tabclose<CR>')
 keymap.set('n', '<C-h>', ':tabprevious<CR>')
@@ -63,11 +64,14 @@ keymap.set('n', '<C-l>', ':tabnext<CR>')
 keymap.set('n', 'th', ':-tabmove<CR>')
 keymap.set('n', 'tl', ':+tabmove<CR>')
 
-keymap.set('n', 'B', ":Telescope buffers<CR>")
-
+-- Map undo to U
 keymap.set('n', 'U', '<C-r>')
 
+-- Telescope bindings
 keymap.set('n', '<C-f>', ':Telescope<CR>')
+keymap.set('n', 'B', ":Telescope buffers<CR>")
+
+-- Open new window with a terminal
 keymap.set('n', 'T', function()
     if vim.keymap.terminal.win_id == nil then
         vim.cmd("40split")
@@ -80,8 +84,10 @@ keymap.set('n', 'T', function()
     end
 end)
 
-
+-- Enable table mode
 keymap.set('n', 'tm', ':TableModeToggle<CR>')
+
+-- Save all tabs and quit
 keymap.set('n', 'QQ', function ()
     vim.cmd("wa")
     vim.cmd("qa")
