074840919f0a265be2d0927c00d39756008c9e95
LutzGebelman <yuraosipov2012@gmail.com>
2023-11-17 15:42:26 +0200

    Removed arrow gliphs

--- lua/lualine-conf.lua
+++ lua/lualine-conf.lua
@@ -8,8 +8,8 @@ require('lualine').setup{
     options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -26,7 +26,7 @@ require('lualine').setup{
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{get_host_user, color = { gui = "bold" }}, 'filename'},
+    lualine_c = {{get_host_user, color = { gui = 'bold' }}, 'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
