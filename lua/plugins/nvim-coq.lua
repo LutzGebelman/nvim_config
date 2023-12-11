3c26eade4f68b6ecbf2b69cc32c688b122472a6e
LutzGebelman <yuraosipov2012@gmail.com>
2023-12-11 03:35:34 +0200

    More reformating

--- lua/plugins/nvim-coq.lua
+++ lua/plugins/nvim-coq.lua
@@ -1,5 +1,6 @@
- return {
-     'ms-jpq/coq_nvim', branch = 'coq', dependencies = {
-         {'ms-jpq/coq.artifacts', branch = 'artifacts'},
-         {'ms-jpq/coq.thirdparty', branch = '3p'}
- }}
+return {
+    'ms-jpq/coq_nvim', branch = 'coq', dependencies = {
+        {'ms-jpq/coq.artifacts', branch = 'artifacts'},
+        {'ms-jpq/coq.thirdparty', branch = '3p'}
+    }
+}
