22312a8593fa1132db7fbe6cc0487ab33789f127
LutzGebelman <yuraosipov2012@gmail.com>
2023-12-19 02:32:12 +0200

    Remade keybindings with <leader> key in mind

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,22 +3,8 @@ keymap.terminal = {
     bid = nil
 }
 
--- WARN: No clue what this is for, so gona leave this here untill something breaks
-
--- local opts = {
---     mode = 'n', -- NORMAL mode
---     -- prefix: use '<leader>f' for example for mapping everything related to finding files
---     -- the prefix is prepended to every mapping part of `mappings`
---     prefix = '',
---     buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
---     silent = true, -- use `silent` when creating keymaps
---     noremap = true, -- use `noremap` when creating keymaps
---     nowait = false, -- use `nowait` when creating keymaps
---     expr = false, -- use `expr` when creating keymaps
--- }
-
-
-keymap.set('n', '.', ':UndotreeToggle<CR>')
+keymap.set('n', '\\', '<leader>')
+keymap.set('n', '<leader>.', ':UndotreeToggle<CR>')
 
 -- open the directory tree
 keymap.set('n', '<C-\\>', ':Oil<CR>')
@@ -26,15 +12,6 @@ keymap.set('n', '<C-\\>', ':Oil<CR>')
 -- Tabulation
 keymap.set('v', '<Tab>', ':s/./\\ \\ \\ \\ \\0/<CR>:nohl<CR>') -- Add a tab to each selected line
 keymap.set('v', '<S-Tab>', ':s/\\ \\{4\\}//<CR>:nohl<CR>') -- Remove a tab from each selected line
--- NOTE: I have no use for this for now
--- keymap.set('n', '<F8>', ':lua require'dap'.toggle_breakpoint()<CR>')
--- keymap.set('n', '<F5>', ':lua require'dap'.continue()<CR>')
--- keymap.set('n', '<F6>', function ()
---     require('dap.ext.vscode').load_launchjs('${workspaceFolder}/launch.json')
--- end)
--- keymap.set('n', '<F10>', ':lua require'dap'.step_over()<CR>')
--- keymap.set('n', '<F11>', ':lua require'dap'.step_into()<CR>')
--- keymap.set('n', 'd-o', ':lua require'dap'.repl.open()<CR>')
 
 keymap.set('n', '<esc>', ':sil nohl<CR>') -- Dismiss the highlight 
 keymap.set('t', '<esc>', '<C-\\><C-n>') -- Exit terminal input on ESC
@@ -60,20 +37,22 @@ keymap.set({ 'n', 'v' }, 'H', '<home>')
 keymap.set({ 'n', 'v' }, 'L', '<end>')
 
 -- tabs controll
-keymap.set('n', 'tt', ':tabnew<CR>')
-keymap.set('n', 'tq', ':tabclose<CR>')
-keymap.set('n', 'th', ':tabprevious<CR>')
-keymap.set('n', 'tl', ':tabnext<CR>')
+-- keymap.set('n', '<leader>tt', ':tabnew<CR>')
+-- keymap.set('n', '<leader>tq', ':tabclose<CR>')
+-- keymap.set('n', '<leader>th', ':tabprevious<CR>')
+-- keymap.set('n', '<leader>tl', ':tabnext<CR>')
 
 -- Map undo to U
 keymap.set('n', 'U', '<C-r>')
 
 -- Telescope bindings
-keymap.set('n', '<C-f>', ':Telescope<CR>')
-keymap.set('n', '<C-b>', ':Telescope buffers<CR>')
-
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- Open new window with a terminal
-keymap.set('n', 'T', function()
+keymap.set('n', '<leader>tt', function()
     if vim.keymap.terminal.bid == nil then
         vim.cmd('40split')
         vim.cmd('wincmd j')
@@ -88,7 +67,7 @@ keymap.set('n', 'T', function()
 end)
 
 -- Enable table mode
-keymap.set('n', 'tm', ':TableModeToggle<CR>')
+keymap.set('n', '<leader>tm', ':TableModeToggle<CR>')
 
 -- Save all tabs and quit
 keymap.set('n', 'QQ', function ()
