2c9c419a502f2517655043295abb587bc0b95b75
LutzGebelman <yuraosipov2012@gmail.com>
2024-03-18 01:36:16 +0200

    Disabled line wrap

--- init.lua
+++ init.lua
@@ -1,25 +1,24 @@
-local op = vim.o
-local gop = vim.g
+local vim = vim
 
-gop.loaded_netrw = 1
-gop.loaded_netrwPlugin = 1
-gop.nohlsearch = false
-op.linebreak = false
-op.termguicolors = true
-op.number = true
-op.rnu = true
-op.updatetime = 300
-op.signcolumn = 'yes'
-op.tabstop = 4
-op.shiftwidth = 4
-op.expandtab = true
-op.termbidi = true
-op.cursorline = true
-op.colorcolumn = "80"
-
+vim.o.loaded_netrw = 1
+vim.o.loaded_netrwPlugin = 1
+vim.o.nohlsearch = false
+vim.o.linebreak = true
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.rnu = true
+vim.o.updatetime = 300
+vim.o.signcolumn = 'yes'
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.termbidi = true
+vim.o.cursorline = true
+vim.o.colorcolumn = "80"
+vim.o.wrap = false
+-- vim.cmd("set nowrap")
 
 require('plugins')
-
 require('keybindings')
 require('theme')
 require('lualine-conf')
